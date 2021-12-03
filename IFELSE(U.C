368cd3d5431d8d35b5a8c1dcbb1ea2cf144bc231
aniket kumar <77187170+ani812001@users.noreply.github.com>
2021-12-03 12:17:55 +0530

    Add files via upload

---
+++ IFELSE(U.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+	int age;
+	clrscr();
+
+	printf("Enter age..!\n");
+	scanf("%d",&age);
+
+	if (age>=18)
+	{
+		printf("Eligible for vote");
+	}
+	else
+	{
+		printf("Not eligible for vote");
+	}
+	getch();
+}
