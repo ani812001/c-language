0c5a9d78e56abb2c8979c2186ab9dc6edae294c3
aniket kumar <77187170+ani812001@users.noreply.github.com>
2021-12-03 16:48:59 +0530

    Add files via upload

---
+++ NESTEDIF.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10,b = 5,c=50;
+	clrscr();
+	if(a>b)
+	{
+		if(a>c)
+		{
+			printf("%d",a);
+		}
+		else
+		{
+			printf("%d",c);
+		}
+	}
+	else
+	{
+		if(b>c)
+		{
+			printf("%d",b);
+		}
+		else
+		{
+			printf("%d",c);
+		}
+	}
+	getch;
+}
+
